f9ff6a3fcfac5b6b752f8d161a60aa229dde35ba
GamalielWP <board.dabil@gmail.com>
2022-10-01 14:59:22 +0700

    Add Modal

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -46,7 +46,28 @@ class CourseController extends Controller
                 $btn = '
                     <a class="btn btn-success btn-sm" href="/edit-index/'.$course->id.'"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
                     |
-                    <a class="btn btn-danger btn-sm" href="/delete-course/'.$course->id.'"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                    <a class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteBackdrop"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+
+                    <div class="modal fade" id="deleteBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="deleteBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="deleteBackdropLabel">Peringatan !</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Anda yakin menghapus data ini ?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                                    <a href="/delete-course/'.$course->id.'" class="btn btn btn-danger">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                        Hapus
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 ';
 
                 return $btn;
